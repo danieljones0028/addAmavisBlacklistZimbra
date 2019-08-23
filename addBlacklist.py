f992339ecc4308d63e8089a3dc4376d147ff4088
Daniel Jones <danieljones028@gmail.com>
2019-08-23 11:37:28 -0300

    new addBlacklist

--- addBlacklist.py
+++ addBlacklist.py
@@ -1,214 +1,221 @@
 # -*- coding: utf-8 -*-
-#
-# WILL BE RECEIVED FROM THE RUNDECK A FILE, WHICH CONTAINS THE NAMES OF DOMAINS TO BE INCLUDED IN THE RBL OF AMAVIS
-# RUNDECK WILL CHANGE ANY NAME GIVEN TO THE FILE TO amavisnewchecklist_toip AND PLACE IT IN / tmp SO
-# GUARANTEE THAT THE CONSULTATION FILE WILL BE AVAILABLE TO SCRIPT
+
 import subprocess
-from subprocess import check_output, CalledProcessError, PIPE
 import os
 import sys
 import socket
-
 import logging
-logging.basicConfig(filename='/opt/pyAntispam/log/pyAddblacklist.log', format='%(asctime)s %(levelname)s %(message)s', level=logging.INFO)
+import dns.resolver #pip install dnspython
 
-amavis_domainlist = ""
-command_awk2 = "'{print $2}'"
-command_sedwhitelines = "'/^$/d'"
-command_sednewstart = "'s/^/@/g'"
-domain = input(str('Digite o nome do seu dominio: '))
-# domain = 'tchucas.net'
-####PATH ZIMBRA
-blacklist_file = '/opt/zimbra/conf/postfix_blacklist'
-recipient_file = '/opt/zimbra/conf/postfix_recipient_access'
-postmap = '/opt/zimbra/postfix/sbin/postmap'
+dominio = input(str('Digite o nome do seu dominio, entre aspas: '))
 
-####PATH APP
-amavis_newchecklist = '/tmp/amavis_newchecklist'
-amavisnewchecklist_toip = '/tmp/amavisnewchecklist_toip'
-amavisnewchecklist_todomain = '/tmp/amavisnewchecklist_todomain'
+# Path's
+amavis_novalistadeverificacao = '/tmp/amavis_novalistadeverificacao'
+amavis_novalistadeverificacaoip = '/tmp/amavis_novalistadeverificacaoip'
 
-#####CODE:
-def refresh_newlists():
+# Zimbra Path's
+if os.path.exists('/opt/zimbra/conf/postfix_blacklist'):
+    postfix_blacklistfile = '/opt/zimbra/conf/postfix_blacklist'
+else:
+    subprocess.call(['touch', '/opt/zimbra/conf/postfix_blacklist'], shell=True)
+# Postmap
+if os.path.exists('/opt/zimbra/postfix/sbin/postmap'):
+    postmap = '/opt/zimbra/postfix/sbin/postmap'
+elif os.path.exists('/opt/zimbra/common/sbin/postmap'):
+    postmap = '/opt/zimbra/common/sbin/postmap'
+else:
+    logging.error('postmap nao encontrado...')
+    print('caminho para o postmap nao encontrado, o Zimbra esta instalado?')
+    sys.exit(1)
+# Zmprov
+if os.path.exists('/opt/zimbra/bin/zmprov'):
+    zmprov = '/opt/zimbra/bin/zmprov'
+else:
+    logging.error('zmprov nao encontrado...')
+    print('caminho para o zmprov nao encontrado, o Zimbra esta instalado?')
+    sys.exit(1)
+
+# Commands
+cmd_awk2 = "'{print $2}'"
+cmd_apagalinhasembranco = "'/^$/d'"
+
+# Logs
+logging.basicConfig(filename='/opt/pyAntispam/log/pyaddblacklist.log', format='%(asctime)s %(levelname)s %(message)s', level=logging.INFO)
+
+# 1
+def listadeverificacao_tratada():
     try:
-        refresh = 'cut -d@ -f2 %s | sort | uniq | sed %s > %s' % (amavis_newchecklist, command_sednewstart, amavisnewchecklist_toip)
-        subprocess.check_call(refresh, shell=True)
-        refresh = 'cut -d@ -f2 %s | sort | uniq > %s' % (amavis_newchecklist, amavisnewchecklist_todomain)
-        subprocess.check_call(refresh, shell=True)
-    except TypeError as e:
-        print(e)
-        logging.error(e)
 
-def getlist_amavis():
-    getlist_amavis = 'zmprov gd %s amavisBlacklistSender | grep -v %s | awk %s | sed %s > /tmp/amavis_%s_blacklist' % (domain, domain,command_awk2, command_sedwhitelines, domain)
-    subprocess.call(getlist_amavis, shell=True)
-    if os.path.exists('/tmp/amavis_%s_blacklist' % (domain)):
-        domain_list = ('/tmp/amavis_%s_blacklist' % (domain))
-        global amavis_domainlist
-        amavis_domainlist = open(domain_list, 'r').read().split('\n')
-    else:
-        print('Current amavis list file not found please contact support')
-        logging.warning('Current amavis list file not found please contact support')
-        sys.exit(1)
+        if os.path.exists(amavis_novalistadeverificacao):
+            amavis_listadeverificacao = open(amavis_novalistadeverificacao, 'r').read().split('\n')
+            amavis_novalista = []
+            amavis_listadeverificacao = sorted(amavis_listadeverificacao)
+            for item in amavis_listadeverificacao:
+                try:
+                    for dominio in item.splitlines():
+                        k, v = dominio.split("@", 2)
+                        amavis_novalista.append(v)
+                        logging.info('adicionando item a lista de verificacao: %s' % (v))
+                except ValueError as e:
+                    logging.error(e)
+                    amavis_novalista.append(dominio)
+                    logging.info('adicionando item a lista de verificacao: %s' % (dominio))
 
-def search_domain():
-    global amavis_domainlist
-    if os.path.exists(amavisnewchecklist_todomain):
-        new_domainline = open(amavisnewchecklist_todomain, 'r').read().split('\n')
-    else:
-        print('Query file not found, contact support.')
-        logging.warning('Query file not found, contact support.')
-        sys.exit(1)
-    # f = open('/dev/null', 'w')
-    for add_domain in new_domainline:
-        for domainlist in amavis_domainlist:
-            if domainlist != "":
-                path_file = '/tmp/amavis_%s_blacklist' % (domain)
-                audit_name = subprocess.Popen(['grep', add_domain, path_file], stdout=subprocess.PIPE)
-                audit_name.communicate()[0]
-                # sys.stdout = f
-                exit_code = audit_name.returncode
-                if exit_code == 1:
-                    print('Adding the domain: %s' % (add_domain))
-                    logging.info('Adding the domain: %s' % (add_domain))
-                    action_finder = 'zmprov md %s +amavisBlacklistSender %s' % (domain, add_domain)
-                    subprocess.call(action_finder, shell=True)
-                    getlist_amavis()
+        lista_unica = []
 
-def getipdomain_listcurrent():
+        for item in amavis_novalista:
+            if item not in lista_unica:
+                logging.info('removendo itens repetidos na lista: %s' % (item))
+                lista_unica.append(item)
+
+        lista_unica = sorted(lista_unica)
+
+    except Exception as e:
+        print('definicao listadeverificacao_tratada erro: %s' % (e))
+        logging.error('definicao listadeverificacao_tratada erro: %s' % (e))
+
+    return lista_unica
+# 2
+def amavis_listatual():
+    amavis_coletalistatual = 'zmprov gd %s amavisBlacklistSender | grep -v %s | awk %s | sed %s > /tmp/amavis_%s_blacklist' % (dominio, dominio, cmd_awk2, cmd_apagalinhasembranco, dominio)
+    subprocess.call(amavis_coletalistatual, shell=True)
     try:
-        if os.path.exists(blacklist_file):
-            blacklist_current = open(blacklist_file, 'r').read().split('\n')
-            current_iplist = []
-            for line in blacklist_current:
-                if line != "":
-                    for ip in line.splitlines():
-                        k, v = ip.split(" ", 1)
-                        current_iplist.append(k)
-            current_iplist = list(dict.fromkeys(current_iplist))
-            return current_iplist
-    except TypeError as e:
-        print(e)
-        logging.warning(e)
+        if os.path.exists('/tmp/amavis_%s_blacklist' % (dominio)):
+            amavis_listatual_dominios = ('/tmp/amavis_%s_blacklist' % (dominio))
+            amavis_listatual_bruta = open(amavis_listatual_dominios, 'r').read().split('\n')
+            amavis_listatual_bruta = sorted(amavis_listatual_bruta)
 
-def getipdomain_liststaging():
-    try:
-        if os.path.exists(amavisnewchecklist_toip):
-            checklist = open(amavisnewchecklist_toip).read().split('\n')
-            new_iplist = []
-            for domain in checklist:
-                if domain != "":
-                    if domain.find('@') > -1:
-                        for ip in domain.splitlines():
-                            k, v = ip.split('@', 1)
-                            try:
-                                if socket.gethostbyname(v):
-                                    v = socket.gethostbyname(v)
-                                    new_iplist.append(v)
-                            except Exception as e:
-                                logging.warning('reverso para %s não encontrado' % (v))
-                                # print(e)
-            new_iplist = list(dict.fromkeys(new_iplist))
-            return new_iplist
-    except TypeError as e:
-        logging.warning(e)
-        # print(e)
+            amavis_listatual = []
 
-def comparation_lists():
-    try:
-        current_iplist = getipdomain_listcurrent()
-        new_iplist = getipdomain_liststaging()
-        if len(current_iplist) and len(new_iplist) > 0:
-            ipsasereminseridos = []
-            for ipnews in new_iplist:
-                if ipnews != "":
-                    if ipnews not in current_iplist:
-                        ipsasereminseridos.append(ipnews)
-            print('lista a ser inserida %s' % (ipsasereminseridos))
-            logging.info('lista a ser inserida %s' % (ipsasereminseridos))
-            ipsasereminseridos = list(dict.fromkeys(ipsasereminseridos))
-            return ipsasereminseridos
+            for item in amavis_listatual_bruta:
+                if item != "":
+                    amavis_listatual.append(item)
+                    logging.info('adicionando item a lista atual: %s' % (item))
         else:
-            logging.info('não existem novas entradas ip')
-    except TypeError as e:
-        print(e)
-        logging.warning(e)
-
-def refresh_config():
-    try:
-        update = '%s %s' % (postmap, blacklist_file)
-        subprocess.check_call(update, shell=True)
-    except TypeError as e:
-        print(e)
-        logging.error(e)
-    try:
-        update = '%s %s' % (postmap, recipient_file)
-        subprocess.check_call(update, shell=True)
-    except TypeError as e:
-        print(e)
-        logging.error(e)
-
-def production_list():
-    try:
-        ipsasereminseridos = comparation_lists()
-        if len(ipsasereminseridos) > 0:
-            logging.info('serão incluidos %s ip' % (len(ipsasereminseridos)))
-            for ipadd in ipsasereminseridos:
-                insert = open(blacklist_file, 'a')
-                insert.write('%s REJECT\n' % (ipadd))
-                insert.close()
-                print('incluindo ip: %s' % (ipadd))
-                logging.info('incluindo ip: %s' % (ipadd))
-            print('processo finalizado com sucesso.')
-            logging.info('processo finalizado com sucesso.')
-            refresh_config()
-        else:
-            print('não existem novas entradas de ip')
-            print('processo finalizado com sucesso.')
-            logging.info('não existem novas entradas de ip')
-            logging.info('processo finalizado com sucesso.')
-    except TypeError as e:
-        print(e)
-        logging.error(e)
-
-def adddomain_recipiens():
-    global amavis_domainlist
-    try:
-        if os.path.exists(amavisnewchecklist_todomain):
-            staging_domainlist = open(amavisnewchecklist_todomain, 'r').read().split('\n')
-            domain_list = ('/tmp/amavis_%s_blacklist' % (domain))
-            amavis_domainlist = open(domain_list, 'r').read().split('\n')
-        else:
-            print('Query file not found, contact support.')
-            logging.warning('Query file not found, contact support.')
+            print('Lista atual não encontrada. Chame o suporte.')
+            logging.error('Lista atual não encontrada. Chame o suporte.')
             sys.exit(1)
-        print(len(amavis_domainlist))
-        print(len(staging_domainlist))
-        if len(amavis_domainlist) and len(staging_domainlist) > 0:
-            nomesasereminseridos = []
-            for namenews in staging_domainlist:
-                if namenews != "":
-                    if namenews not in amavis_domainlist:
-                        nomesasereminseridos.append(namenews)
-            print('nome a serem inseridos: %s' % (nomesasereminseridos))
-            logging.info('nome a serem inseridos: %s' % (nomesasereminseridos))
-            for addname in nomesasereminseridos:
-                insert = open(recipient_file, 'a')
-                insert.write('%s User 550 Unknown\n' % (addname))
-                insert.close()
-                print('incluindo nome: %s' % (addname))
-                logging.info('incluindo nome: %s' % (addname))
-        else:
-            print('não existem novas entradas de domino')
-            print('processo finalizado com sucesso.')
-            logging.info('não existem novas entradas de dominio')
-            logging.info('processo finalizado com sucesso.')
-    except TypeError as e:
-        print(e)
-        logging.error(e)
 
-refresh_newlists()
-getlist_amavis()
-search_domain()
-adddomain_recipiens()
-production_list()
+        amavis_listatual = sorted(amavis_listatual)
+
+        return amavis_listatual
+
+    except Exception as e:
+        print('definicao amavis_listatual erro: %s' % (e))
+        logging.error('definicao amavis_listatual erro: %s' % (e))
+# 3
+def comparando_listas(atual, checagem):
+    listapara_adesao = []
+    try:
+        for item in checagem:
+            if item not in atual:
+                logging.info('adicionando item para lista de adesao: %s' % (item))
+                listapara_adesao.append(item)
+    except Exception as e:
+        print('definicao comparando_listas erro: %s' % (e))
+        logging.error('definicao comparando_lista erro: %s' % (e))
+
+    listapara_adesao = sorted(listapara_adesao)
+
+    return listapara_adesao
+# 4
+def lista_ips(data):
+    try:
+        lista_ips = []
+        lista_mx = []
+        lista_unica = []
+        for mxs in data:
+            for mx in dns.resolver.query(mxs, 'MX'):
+                mx = mx.to_text()
+                try:
+                    for nome_mx in mx.splitlines():
+                        k, v = nome_mx.split(" ", 2)
+                        lista_mx.append(v)
+                        logging.info('MX coletado: %s' % (v))
+                except ValueError as e:
+                    logging.error(e)
+
+            for ips in lista_mx:
+                try:
+                    if socket.gethostbyname(ips):
+                        ip = socket.gethostbyname(ips)
+                        lista_ips.append(ip)
+                except Exception as e:
+                    logging.warning('nao foi possivel resolver o ip do dominio: %s' % (ips))
+                    print('nao foi possivel resolver o ip do dominio: %s' % (ips))
+    except Exception as e:
+        print('definicao lista_ips erro: %s' % (e))
+        logging.error('definicao lista_ips erro: %s' % (e))
+
+    for item in lista_ips:
+        if item not in lista_unica:
+            logging.info('removendo itens repetidos na lista: %s' % (item))
+            lista_unica.append(item)
+
+    lista_ip = sorted(lista_unica)
+
+    return lista_ip
+# 5
+def postfix_blacklist(data):
+# pegar a lista atual
+    arquivo_blacklist = 'cat %s > %s' % (postfix_blacklistfile, amavis_novalistadeverificacaoip)
+    subprocess.call(arquivo_blacklist, shell=True)
+    blacklist_atual = open(amavis_novalistadeverificacaoip, 'r').read().split('\n')
+    blacklist_atual = sorted(blacklist_atual)
+    del blacklist_atual [0]
+
+    lista_unica = []
+    lista_ipatual = []
+    lista_ipadesao = []
+# O arquivo é alimentado IP REJECT por linha. Exemplo 1.1.1.1 REJECT
+    try: # Separa IP REJECT
+        for ip in blacklist_atual:
+            if ip != '':
+                for ip_atual in ip.splitlines():
+                    k, v = ip_atual.split(" ", 1)
+                    lista_ipatual.append(k)
+
+        for item in lista_ipatual:
+            if item not in lista_unica:
+                logging.info('removendo itens repetidos caso existam na lista: %s' % (item))
+                lista_unica.append(item)
+
+        for ip_novo in data:
+            if ip_novo not in lista_unica:
+                lista_ipadesao.append(ip_novo)
+                logging.info('adicionando ip: %s a lista de adesao.' % (ip_novo))
+
+        if not len(lista_ipadesao) == 0:
+            for ip_adesao in lista_ipadesao:
+                print('incluindo ip: %s' % (ip_adesao))
+                cmd_shell = "echo '%s REJECT' >> %s" % (ip_adesao, postfix_blacklistfile)
+                subprocess.call(cmd_shell, shell=True)
+                logging.info('incluindo ip: %s' % (ip_adesao))
+
+            cmd_postmap = '%s %s' % (postmap, postfix_blacklistfile)
+            subprocess.call(cmd_postmap, shell=True)
+
+        else:
+            logging.warning('nao existem novas entradas ips')
+            print('nao existem novas entradas ips.')
+
+    except Exception as e:
+        print('definicao postfix_blacklist erro: %s' % (e))
+        logging.error('definicao postfix_blacklist erro: %s' % (e))
+# 6
+def amavis_blacklistsender(data):
+    try:
+        if not len(data) == 0:
+            for dominio_adesao in data:
+                cmd_zmprov = '%s md %s +amavisBlacklistSender %s' % (zmprov, dominio, dominio_adesao)
+                subprocess.call(cmd_zmprov, shell=True)
+        else:
+            logging.info('nao existem novas entradas dominio.')
+            print('nao existem novas entradas dominio.')
+    except Exception as e:
+        print('definicao amavis_blacklistsender erro: %s' % (e))
+        logging.error('definicao amavis_blacklistsender erro: %s' % (e))
+
+lista_ips(comparando_listas(amavis_listatual(), listadeverificacao_tratada()))
+postfix_blacklist(lista_ips(comparando_listas(amavis_listatual(), listadeverificacao_tratada())))
+amavis_blacklistsender(comparando_listas(amavis_listatual(), listadeverificacao_tratada()))
